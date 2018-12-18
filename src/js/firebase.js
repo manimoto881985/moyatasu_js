f2555e7da3f12668ea6108c43342a30968384476
manimoto881985 <manimoto88@gmail.com>
2018-12-18 10:45:35 +0900

    set firebase setting on dotenv

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-  apiKey: "AIzaSyBEgc7VMkkdDw4YVTlHSwcJZ3_1dIw_BwA",
-  authDomain: "myfirebasechatapp-5ba43.firebaseapp.com",
-  databaseURL: "https://myfirebasechatapp-5ba43.firebaseio.com",
-  projectId: "myfirebasechatapp-5ba43",
-  storageBucket: "myfirebasechatapp-5ba43.appspot.com",
-  messagingSenderId: "1012451440869"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config)
